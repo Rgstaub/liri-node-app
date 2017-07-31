88dc0487e711919392bd72229d3ba83fedd23ab9
Rgstaub <rgstaub@gmail.com>
2017-07-30 21:26:07 -0400

    Finishing the twitter bot

--- js/twitter.js
+++ js/twitter.js
@@ -2,14 +2,10 @@ let Twitter = require('twitter');
 //let exports = module.exports = {};
 let keys = require('./keys.js');
 
-console.log("My Tweets:");
 
 let error = function(err, response, body) {
   console.log('ERROR [%s]', err);
 };
-var success = function (data) {
-    console.log('Data [%s]', data);
-};    
 
 var twitter = new Twitter(keys.twitterKeys);
 twitter.get('search/tweets', {q: "bobloblawatty"}, function(err, body, response) {
@@ -17,19 +13,45 @@ twitter.get('search/tweets', {q: "bobloblawatty"}, function(err, body, response)
     error(err, body, response);
     return;
   };
-  // for (let i = 0; i < tweets.statuses.length; i++) {
-  //   console.log((tweets.statuses[i].text));
-  // }
-  
-  console.log(body.statuses[0]);
-  let tweetDate = body.statuses[0].created_at;
-  tweetDate = tweetDate.substring(0, 10);
-  let tweetText = body.statuses[0].text;
-  let retweetedFrom = body.statuses[0].retweeted_status.user.name
-  console.log(tweetDate);
-  console.log(tweetText);
-  console.log(retweetedFrom)
+  console.log(`
+                              ▀▄░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                            ░░▀▄▀▄░░░░░░░░░░░░░░░░░░▄▀▄▀▀▄░░▄▀░░
+                            ░░▀▄▀▄▀▄░░░░░░░░░░░░░░▄▀▄▀▄▀▄▀▄▀░░▄▄
+                            ░░░░▀▄▀▄▀▄░░░░░░░░░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░
+                            ░░░░▀▄▀▄▀▄▀▄▀▄░░░░░░▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░
+                            ░░▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░
+                            ░░░░▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀▄▀░░
+                            ░░░░░░▀▄▀▄▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀░░░░
+                            ░░░░▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀░░░░
+                            ░░░░░░▀▄▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀░░░░░░
+                            ░░░░▒▒▒▒▀▄▀▄▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀▒▒░░░░
+                            ▒▒▒▒▒▒▒▒▒▒▀▄▀▄▀▄▄▀▄▀▄▀▄▀▄▀▄▀▒▒▒▒▒▒▒▒
+                            ▒▒▒▒▒▒▒▒▒▒▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▒▒▒▒▒▒▒▒▒▒
+                            ▒▒▒▒▒▒▒▒▄▀▄▀▄▀▄▀▄▀▄▀▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                            ▒▒▒▒▒▒▄▀▄▀▄▀▄▀▄▀▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                            ▒▒▄▀▄▀▄▀▄▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+                              ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+    ====================================================================================
+                                          MY TWEETS
+    ====================================================================================
+  `)
+  for (let i = 0; i < body.statuses.length; i++) {
+    let tweetDate = body.statuses[i].created_at;
+    tweetDate = tweetDate.substring(0, 10);
+    let tweetText = body.statuses[i].text;
+    let retweetedFrom = "";
+    if (body.statuses[i].retweeted_status) {
+      retweetedFrom = body.statuses[i].retweeted_status.user.name;
+    }
+    console.log(`
+    ------------------------------------------------------------------------------------
+                                          ${tweetDate}
 
+    "${tweetText}"
+
+    - ${retweetedFrom}
+    ___________________________________________________________________________________`)
+  }
 } )
 
-module.exports.tweets;
+module.exports.twitter;
