3e67d109c8edb211f4d309c8e475c985295071c0
Rgstaub <rgstaub@gmail.com>
2017-08-02 16:21:57 -0400

    ...

--- js/movie.js
+++ js/movie.js
@@ -5,7 +5,6 @@ let title = "minions";
 request(`http://www.omdbapi.com/?apikey=40e9cece&s=${title}`, function(err, response, body) {
     let movie = JSON.parse(body, null, 2);
     console.log(movie.Search[0]);
-    
 
 })
  
