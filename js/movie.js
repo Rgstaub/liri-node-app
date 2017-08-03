4f8c49a0d3e98ebeaee011942806c08a0ff9412f
Rgstaub <rgstaub@gmail.com>
2017-08-02 21:54:10 -0400

    Polishing movie.js

--- js/movie.js
+++ js/movie.js
@@ -1,14 +1,57 @@
 var request = require('request');
 
-let title = "minions";
+let title = "";
+for (let i = 3; i < process.argv.length; i++ ) {
+    title += ` ${process.argv[i]}`;
+}
+console.log(`Title: ${title}`);
 
-request(`http://www.omdbapi.com/?apikey=40e9cece&s=${title}`, function(err, response, body) {
-    
-    console.log(body);
+request(`http://www.omdbapi.com/?apikey=40e9cece&t=${title}&type=movie`, function(err, response, body) {
+
+    if (err) {
+        return console.log(err);
+    }
     let movie = JSON.parse(body, null, 2);
-    console.log(movie);
-    let movieTitle = movie.Search[0].Title;
-    let movieYear = movie.Search[0].Year;
+    console.log(movie)
+    let movieTitle = movie.Title;
+    let movieYear = movie.Year;
+    let imdbScore = "";
+        if (movie.imdbRating === "N/A") {
+        imdbScore = "not rated";
+    } else {
+        imdbScore = movie.imdbRating;
+    }
+    let rottenScore = "";
+    if (movie.Ratings[1]) {
+        rottenScore = movie.Ratings[1].Value;
+    } else {
+        rottenScore = "is not rated"
+    }
+    let movieCountry = movie.Country;
+    let movieLang = movie.Language;
+    let moviePlot = movie.Plot;
+    let actors = movie.Actors;
+    let bannerFill = "";
+    let marquis = `${movieTitle} (${movieYear})`;
+    let marquisLength = marquis.length;
+    console.log(marquisLength);
+    for (let j = 0; j < marquisLength; j++) {
+        bannerFill += "~";
+    }
+
+
+    console.log(`
+~~~~~~~~~~~~~~~~~~~~${bannerFill}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-------========<<<<<<<<| ${marquis}) |>>>>>>>>========-------
+~~~~~~~~~~~~~~~~~~~~${bannerFill}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+'${movieTitle}' was released in the ${movieCountry} in ${movieLang}. 
+It is rated ${imdbScore} on IMDB and ${rottenScore} on Rotten Tomatoes.
+
+Starring: ${actors}
+
+Plot: ${moviePlot}
+    `)
 
 })
  
